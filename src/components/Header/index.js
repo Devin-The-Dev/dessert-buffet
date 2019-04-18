8c69f876a131de80e6512b3355e7df76ed10a2b3
Devin Udy <devin.udy@gmail.com>
2019-04-17 20:00:40 -0600

    Pushing to GitHub

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Dessert Buffet!</h1>
+      <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+      <h2>You're at a friends party and host/hostess said to only take one of each. Try to take one of each dessert or else the remaining guests will get mad.</h2>
+    </header>
+  );
+}
+
+export default Header;
